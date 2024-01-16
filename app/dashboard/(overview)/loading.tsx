e273ff8e1074d61ace35be9f469e704819244b4b
connorwarme <connor.warme@gmail.com>
2024-01-16 14:55:07 -0700

    Add metadata titles to each page

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,4 +1,9 @@
 import DashboardSkeleton from "../../ui/skeletons";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 export default function Loading() {
   return <DashboardSkeleton />;
